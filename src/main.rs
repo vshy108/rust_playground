c17715451b52760d94f7e53d8a4af5d25fd672c7
Ho Peng Hou <808926+vshy108@users.noreply.github.com>
2026-07-13 00:11:14 +0800

    chore: adding default-run to the [package] section so cargo run defaults to main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,10 @@ fn main() {
     println!("Sample main.rs read: {}", rust_playground::add(1, 2));
     // `cargo run` to check error
     // cargo clippy --all-targets --all-features -- -D warnings
+
+    // prints the shortest round-trip representation hence 5f64.sqrt() less decimal places
+    // 3.141592653589793
+    println!("{}", std::f64::consts::PI);
+    // 2.23606797749979
+    println!("{}", 5f64.sqrt());
 }
